36a883e1d9b40930068b4d4f7466f27ed7b7d1a6
Djony <killerbot86@gmail.com>
2022-06-26 14:45:15 +0500

    1Задача

--- Program.cs
+++ Program.cs
@@ -65,3 +65,11 @@
 // int num = Convert.ToInt32(Console.ReadLine());
 // int a = num % 10;
 // Console.WriteLine(a);
+
+
+// 1.Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
