796709dc14dbb5f0339b51f5ed0aeff17d4c8da3
Djony <killerbot86@gmail.com>
2022-07-03 22:12:04 +0500

    домашняя работа

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@
 // int a = num % 10;
 // Console.WriteLine(a);
 
-
+// // Домашнее задание,урок 1
 // // 1.Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 // Console.Write("Input first number: ");
@@ -117,3 +117,94 @@
 //     Console.Write(current + " ");
 //     current++;
 // }
+
+// // Лекции
+// //из одномерного массива array из n элементов, требуется найти элемент массива, равный find
+
+// int [] array = { 21, 22, 23, 24, 25, 26, 72, 82, 92, 210, 26,5 };
+// int n = array.Length;
+// int find = 26;
+// int index = 0;
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//     Console.WriteLine(index);
+//     break; //-прерви
+//     }
+//     //index = index + 1;
+//     index++;
+// }
+
+
+// void FillArray(int[] collection)
+
+// {
+//     int lenght = collection.Length;
+//     int index = 0;
+//     while (index < lenght)
+//     {
+//         collection[index] = new Random().Next(1, 10);
+//         //index = index + 1;
+//         index++;
+//     }
+// }
+
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.WriteLine(col[position]);
+//         position++;
+//     }
+// }
+
+// int IndexOf(int[] colection, int find)
+// {
+//     int count = colection.Length;
+//     int index = 0;
+//     int position = 0;
+
+
+//     while (index < count)
+//     {
+//         if(colection[index] == find)
+//         {
+//             position = index;
+//             break;
+//         }
+//         index++;
+//     }
+//     return position;
+// }
+
+// int [] array = new int[10];
+
+// FillArray(array);
+// array[3] = 4;
+// array[8] = 7;
+// PrintArray(array);
+// Console.WriteLine();
+
+// int pos = IndexOf(array, 5);
+// Console.WriteLine(pos);
+
+
+// // Домашнее задание,урок 2
+// //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int a = num / 10 % 10;
+// Console.WriteLine(a);
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = num / 100 % 10;
+    Console.WriteLine(a);
+    if (num < 99)
+    Console.Write("Третьей цифры нет");
